8c729222565d07aa8607c9874289b7cc9d08b9d8
Robert-Mutugi <52399629+Robert-Mutugi@users.noreply.github.com>
2023-12-06 22:04:16 +0000

    error

--- aqua_app/lib/main.dart
+++ aqua_app/lib/main.dart
@@ -6,7 +6,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async{
-  //WidgetFlutterBinding.ensureInitialized();
+  WidgetFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   final fcmToken = await FirebaseMessaging.instance.getToken();
   print(fcmToken);
@@ -95,22 +95,3 @@ class _HiddenDrawerState extends State<HiddenDrawer> {
     );
   }
 }
-
-FirebaseMessaging.getInstance().getToken()
-    .addOnCompleteListener(new OnCompleteListener<String>() {
-        @Override
-        public void onComplete(@NonNull Task<String> task) {
-          if (!task.isSuccessful()) {
-            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
-            return;
-          }
-
-          // Get new FCM registration token
-          String token = task.getResult();
-
-          // Log and toast
-          String msg = getString(R.string.msg_token_fmt, token);
-          Log.d(TAG, msg);
-          Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-        }
-    });
