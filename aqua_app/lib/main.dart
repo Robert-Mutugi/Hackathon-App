703fd160a7d812fd720ddb5260659edf70d60ec9
Robert-Mutugi <52399629+Robert-Mutugi@users.noreply.github.com>
2023-12-06 23:17:28 +0000

    Fix

--- aqua_app/lib/main.dart
+++ aqua_app/lib/main.dart
@@ -5,10 +5,11 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
-  
+void main() async {
+ 
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
