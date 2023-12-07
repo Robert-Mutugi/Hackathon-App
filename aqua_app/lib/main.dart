47c802e0d4287144297afc8be74f891302c9b1b1
Robert-Mutugi <52399629+Robert-Mutugi@users.noreply.github.com>
2023-12-07 12:30:15 +0000

    Firebase stuff

--- aqua_app/lib/main.dart
+++ aqua_app/lib/main.dart
@@ -5,11 +5,42 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() async {
- 
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.android,
+  );
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  await FirebaseMessaging.instance.setAutoInitEnabled(true);
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: true,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  print('User granted permission: ${settings.authorizationStatus}');
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
 }
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
 
+  print("Handling a background message: ${message.messageId}");
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -52,6 +83,9 @@ class _HiddenDrawerState extends State<HiddenDrawer> {
   @override
   void initState() {
     super.initState();
+    FirebaseMessaging.instance.getToken().then((token){
+      print("$token");
+    }); 
     _pages = [
       ScreenHiddenDrawer(
         ItemHiddenMenu(
